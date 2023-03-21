aad51820f8736c1b0df74bc13e88a79ea53ec9bd
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-03-21 00:41:00 -0400

    implementar si se ingresa 2 devuelve II

--- src/aRomano.spec.js
+++ src/aRomano.spec.js
@@ -4,4 +4,7 @@ describe("Prueba", () => {
   it("se ingresa 1 Deberia devolver I", () => {
     expect(aRomano(1)).toEqual('I');
   });
+  it("se ingresa 2 Deberia devolver II", () => {
+    expect(aRomano(2)).toEqual('II');
+  });
 });
